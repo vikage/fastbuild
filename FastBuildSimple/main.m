c4ce5859d7944c1a77a2d408a169f2c89319c703
fsociety <fsociety.noname@gmail.com>
2018-05-27 14:58:44 +0700

    Temp complete

--- FastBuildSimple/main.m
+++ FastBuildSimple/main.m
@@ -7,11 +7,88 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+NSString *currentDIR;
+
+void compileFile(NSString *filePath,NSString *fileName);
+void reBuildBinary(void);
+
+NSString *GetHomeDir()
+{
+    char *home = getenv("HOME");
+    return [[NSString alloc] initWithUTF8String:home];
+}
+
+NSString * GetSystemCall(NSString *cmd)
+{
+    NSString *tempFilePath = [NSString stringWithFormat:@"%@/Documents/Temp.out",GetHomeDir()];
+    NSString *reCmd = [NSString stringWithFormat:@"%@ > %@",cmd,tempFilePath];
+    system(reCmd.UTF8String);
+    
+    NSString *output = [[NSString alloc] initWithContentsOfFile:tempFilePath encoding:NSUTF8StringEncoding error:nil];
+    return [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+NSString *GetFileNameFromFilePath(NSString *filePath)
+{
+    NSArray *components = [filePath componentsSeparatedByString:@"/"];
+    NSString *filePathAndEx = [components lastObject];
+    NSArray *fileComponents = [filePathAndEx componentsSeparatedByString:@"."];
+    return [fileComponents firstObject];
+}
+
+int main(int argc, const char * argv[])
+{
+    currentDIR = GetSystemCall(@"pwd");
+#ifdef DEBUG
+    currentDIR = @"/Users/fsociety/Desktop/TestSwift/";
+#endif
+    
+    printf("[ENV] %s\n",currentDIR.UTF8String);
+    // Get List file modify
+    NSString *commandGetListFileModify = [NSString stringWithFormat:@"cd %@;git status -s | cut -c4- | grep -E \".m$|.swift$\"",currentDIR];
+    NSString *resultListFileModify = GetSystemCall(commandGetListFileModify);
+    NSArray *listFileNameModified = [resultListFileModify componentsSeparatedByString:@"\n"];
+    
+    for (NSString *fileModified in listFileNameModified)
+    {
+        NSString *fullPath = [NSString stringWithFormat:@"%@/%@",currentDIR,fileModified];
+        
+        NSString *fileName = GetFileNameFromFilePath(fileModified);
+        
+        compileFile(fullPath, fileName);
     }
+    
+    reBuildBinary();
+    printf("Done\n");
+    
     return 0;
+}
+
+void compileFile(NSString *filePath,NSString *fileName)
+{
+    printf("Compiling %s\n",fileName.UTF8String);
+    NSString *scriptFileName = @"objc-build.sh";
+    if ([filePath hasSuffix:@"swift"])
+    {
+        scriptFileName = @"swift-build.sh";
+    }
+    
+    NSString *scriptFilePath = [NSString stringWithFormat:@"%@/Documents/fastbuild/%@",GetHomeDir(),scriptFileName];
+    NSString *compileCommand = [[NSString alloc] initWithContentsOfFile:scriptFilePath encoding:NSUTF8StringEncoding error:nil];
+    compileCommand = [compileCommand stringByReplacingOccurrencesOfString:@"${FILENAME}" withString:fileName];
+    compileCommand = [compileCommand stringByReplacingOccurrencesOfString:@"${FILEPATH}" withString:filePath];
+    GetSystemCall(compileCommand);
+}
+
+void reBuildBinary()
+{
+    printf("Rebuilding...\n");
+    NSString *scriptFilePath = [NSString stringWithFormat:@"%@/Documents/fastbuild/rebuild.sh",GetHomeDir()];
+    NSString *rebuildCommand = [[NSString alloc] initWithContentsOfFile:scriptFilePath encoding:NSUTF8StringEncoding error:nil];
+    
+    GetSystemCall(rebuildCommand);
 }
