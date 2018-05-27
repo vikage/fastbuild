1144a887c88fe1fd0b1fecba9169c7ff229e82bb
fsociety <fsociety.noname@gmail.com>
2018-05-27 14:14:36 +0700

    Initial Commit

---
+++ FastBuildSimple/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FastBuildSimple
+//
+//  Created by fsociety on 5/27/18.
+//  Copyright © 2018 fsociety. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
